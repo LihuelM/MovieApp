0ae798318ed56bd831087377fbec6eb623bc8cfb
Lihuel <muracciole.al@gmail.com>
2023-02-14 23:53:48 +0700

    migración a Axios + navegación

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-async function getTrendingMoviesPreview () {
-  const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-  const data = await res.json();
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    'api_key': API_KEY,
+  },
+});
 
+async function getTrendingMoviesPreview () {
+  const { data } = await api('trending/movie/day');
   const movies = data.results;
+
   movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+    const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
 
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
@@ -15,17 +24,16 @@ async function getTrendingMoviesPreview () {
     movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300'+ movie.poster_path);
     
     movieContainer.appendChild(movieImg);
-    trendingPreviewMoviesContainer.appendChild(movieContainer);
+    trendingMoviesPreviewList.appendChild(movieContainer);
   });
 }
 
 async function getCategoriesPreview () {
-  const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
-  const data = await res.json();
-
+  const { data } = await api('genre/movie/list');
   const categories = data.genres;
+  
   categories.forEach(category => {
-    const categoriesPreviewContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+    const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list');
 
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
@@ -37,9 +45,7 @@ async function getCategoriesPreview () {
 
     categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
-    categoriesPreviewContainer.appendChild(categoryContainer);
+    categoriesPreviewList.appendChild(categoryContainer);
   });
 }
 
-getTrendingMoviesPreview();
-getCategoriesPreview();
